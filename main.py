203c98f275e851cfc474304ad25ba78e81c80c3c
saginesl <149161625+saginesl@users.noreply.github.com>
2024-04-30 23:41:24 +0700

    Работа с HTTP-ответами

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from routers import router
+
+app=FastAPI()
+
+app.include_router(router)
+
+@app.on_event("startup")
+async def startup_event():
+    pass
---
+++ models.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class ProductModel(BaseModel):
+    id: Optional[int]
+    name: str
+    price: float
+    description: Optional[str]
